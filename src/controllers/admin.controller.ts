27213f17addcef353450182ca886463bba51df83
M.U <rustamovmuhammadumar2004@gmail.com>
2026-08-09 12:56:37 +0500

    feat: member service model integration

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
+import MemberService from "../models/Member.service";
 
 const adminController: T = {};
+const memberService = new MemberService();
+
 adminController.goHome = (req: Request, res: Response) => {
     try {
         res.send("Home Page");
