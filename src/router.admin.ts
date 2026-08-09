6eb4887b36c1131c4883b6c35b2e83c3e739f7ad
M.U <rustamovmuhammadumar2004@gmail.com>
2026-08-09 14:02:36 +0500

    feat: creating admin member related router get and post endpoints

---
+++ src/router.admin.ts
@@ -0,0 +1,21 @@
+import express from "express";
+const routerAdmin = express.Router();
+import adminController from "./controllers/admin.controller";
+
+// ** Admin Routes **//
+routerAdmin.get("/", adminController.goHome);
+
+routerAdmin
+    .get("/login", adminController.getLogin)
+    .post("/login", adminController.processLogin);
+
+routerAdmin
+    .get("/signup", adminController.getSignup)
+    .post("/signup", adminController.processSignup);
+
+// ** Product Routes **//
+
+// ** User Routes **//
+
+
+export default routerAdmin;
