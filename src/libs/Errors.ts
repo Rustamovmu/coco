0a0d02fec909f11186ab7069cacd02edfbb44bbc
M.U <rustamovmuhammadumar2004@gmail.com>
2026-08-26 20:15:42 +0500

    fix: modify spa login api process

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -18,8 +18,10 @@ export enum Message {
     USED_NICK_PHONE = "Nickname or phone number already in use!",
     NO_MEMBER_NICK = "No member with this nickname!",
     WRONG_PASSWORD = "Wrong password, please try again!",
+    BLOCKED_USER = "You have been blocked, please contact Admin!",
     NOT_AUTHENTICATED = "You are not authenticated, please login first!",
     NOT_AUTHORIZED = "Not authorized to perform this action, please login first!"
+
 }
 
 class Errors extends Error {
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,5 +1,5 @@
 import { shapeIntoMongooseObjectid } from "../libs/config";
-import { MemberType } from "../libs/enums/member.enum";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
 import MemberModel from "../schema/Member.model";
@@ -35,11 +35,17 @@ class MemberService {
         // consider member status in the future
         const member = await this.memberModel
             .findOne(
-                { memberNick: input.memberNick },
-                { memberNick: 1, memberPassword: 1 })
+               {
+                    memberNick: input.memberNick,
+                    memberStatus: { $ne: MemberStatus.DELETE },
+                },
+                { memberNick: 1, memberPassword: 1, memberStatus: 1 })
             .exec();
         if (!member) {
             throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+        }
+        else if (member.memberStatus === MemberStatus.BLOCK) {
+            throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER);
         }
 
         if (!member.memberPassword) {
